d12a8225e9634ddb8ddd9fda5622363e7a2e3c13
Sara Spano <saraspano@live.com>
2026-06-19 08:56:06 +0200

    fix

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,6 @@
 import { motion } from 'framer-motion';
 import SEO from "../lib/seo";
 
-import WaveBackground from '../components/WaveBackground';
-
-
-
-
 import IdentityHero from '../components/IdentityHero';
 import TechProfileSection from '../components/TechProfileSection';
 import MyValuesSection from '../components/MyValuesSection';
@@ -23,23 +18,17 @@ export default function About(): React.ReactElement {
                 <IdentityHero />
             </motion.div>
 
-            <motion.div>
-                <WaveBackground />
-
-                <motion.div className="relative z-10">
-                    <TechProfileSection />
-                </motion.div>
-
-                <motion.div className="relative z-10">
-                    <MyValuesSection />
-                </motion.div>
-
-                <motion.div className="relative z-10">
-                    <CtaExperiencesSection />
-                </motion.div>
-
+            <motion.div className="relative z-10">
+                <TechProfileSection />
             </motion.div>
 
+            <motion.div className="relative z-10">
+                <MyValuesSection />
+            </motion.div>
+
+            <motion.div className="relative z-10">
+                <CtaExperiencesSection />
+            </motion.div>
 
         </>
 
